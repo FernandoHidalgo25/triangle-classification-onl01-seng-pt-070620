28934d7d482a38e3d821b42869052806f75e3485
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-09 03:55:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,8 @@ class Triangle
       raise TriangleError
     elsif @side1 == @side1 && @side2 == @side3
       :equilateral
-    elsif @side1 == @side2 && @side2 == @side3
-      :isosceles
+    elsif @side1 == @side2 && @side2 != @side3
+      :equilateral
     elsif @side2 == @side3
       :isosceles
     elsif @side1 == @side3
